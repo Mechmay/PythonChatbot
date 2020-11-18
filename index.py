87cc6a95da9df6e4c29cd192ef35c0bdf1c9c97a
Gift Balogun <blgnbalogun53@gmail.com>
2020-11-18 14:19:39 +0100

    Upgrade

--- index.py
+++ index.py
@@ -1,6 +1,19 @@
-#!/usr/bin/python3
-import os
+print("Welcome To My Chat Support, Follow Me Below")
+
+import nltk
 import aiml
+import os
+# nltk.download() # for downloading packages
+
+import numpy as np
+import random
+import string # to process standard python strings
+import bs4 as bs  
+import urllib.request  
+import re  
+import lxml
+import warnings
+warnings.filterwarnings('ignore')
 
 BRAIN_FILE="brain.csv"
 
@@ -20,13 +33,89 @@ else:
     print("Saving brain file: " + BRAIN_FILE)
     k.saveBrain(BRAIN_FILE)
 
-# Endless loop which passes the input to the bot and prints
-# its response
-while True:
-    input_text = input(" Hey Guest User Send me a Message > ")
-    response = k.respond(input_text)
-    print(" Jurini Fitness Bot  >  " + response)
+#raw = urllib.request.urlopen('https://royalbcodeventures.com.ng/text.txt')  
+#raw=raw.read()
 
-    if input_text == 'quit': 
-       print ("Thank you for visiting.")
-       break    
+
+raw = urllib.request.urlopen('https://www.mayoclinic.org/diseases-conditions/chronic-kidney-disease/symptoms-causes/syc-20354521')  
+raw = raw.read() 
+
+raw = bs.BeautifulSoup(raw, 'lxml')
+
+article_paragraphs = raw.find_all('p')
+
+raw = ''
+
+for para in article_paragraphs:  
+    raw += para.text
+
+raw=raw.lower()# converts to lowercase
+sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
+word_tokens = nltk.word_tokenize(raw)# converts to list of words
+
+sent_tokens[:2]
+word_tokens[:5]
+
+lemmer = nltk.stem.WordNetLemmatizer()
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
+GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+
+# Checking for greetings
+def greeting(sentence):
+    """If user's input is a greeting, return a greeting response"""
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+# Generating response
+def response(user_response):
+    robo_response=''
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf==0):
+        robo_response=robo_response+"I am sorry! I don't understand you"
+        return robo_response
+    else:
+        robo_response = robo_response+sent_tokens[idx]
+        return robo_response
+
+
+flag=True
+print("Chat Support: My name is Jurini. I will answer your queries about Chatbots. If you want to exit, type Bye!")
+
+while(flag==True):
+    user_response = input("Master Message > ")
+    user_response=user_response.lower()
+    if(user_response!='bye'):
+        if(user_response=='thanks' or user_response=='thank you' ):
+            flag=False
+            print("Chat Support: You are welcome..")
+        else:
+            if(greeting(user_response)!=None):
+                print("Chat Support: "+greeting(user_response))
+            else:
+                sent_tokens.append(user_response)
+                word_tokens=word_tokens+nltk.word_tokenize(user_response)
+                final_words=list(set(word_tokens))
+                print("Chat Support: ",end="")
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+    else:
+        flag=False
+        print("ROBO: Bye! take care..") 
