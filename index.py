dbb3d5649fa07417ab68e010556ab185101a0452
Gift Balogun <blgnbalogun53@gmail.com>
2021-01-11 19:54:32 +0100

    Final Update

--- index.py
+++ index.py
@@ -1,13 +1,21 @@
 print("Welcome To My Chat Support, Follow Me Below")
 
+#State 1
 import nltk
 import aiml
 import os
-# nltk.download() # for downloading packages
 
+#State 2
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk, Image
+import numpy
+
+#State 1
 import numpy as np
 import random
-import string # to process standard python strings
+import string
 import bs4 as bs  
 import urllib.request  
 import re  
@@ -32,10 +40,6 @@ else:
     k.bootstrap(learnFiles="fitness.aiml", commands="") #Loads the fitness.aiml file
     print("Saving brain file: " + BRAIN_FILE)
     k.saveBrain(BRAIN_FILE)
-
-#raw = urllib.request.urlopen('https://royalbcodeventures.com.ng/text.txt')  
-#raw=raw.read()
-
 
 raw = urllib.request.urlopen('https://www.mayoclinic.org/diseases-conditions/chronic-kidney-disease/symptoms-causes/syc-20354521')  
 raw = raw.read() 
@@ -72,7 +76,6 @@ def greeting(sentence):
     for word in sentence.split():
         if word.lower() in GREETING_INPUTS:
             return random.choice(GREETING_RESPONSES)
-
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -119,3 +122,80 @@ while(flag==True):
     else:
         flag=False
         print("ROBO: Bye! take care..") 
+
+#Stage 2: Image Classificaion
+
+#load the trained model to classify the images
+
+from keras.models import load_model
+model = load_model('model.h5')
+
+#dictionary to label all the CIFAR-10 dataset classes.
+
+classes = { 
+    0:'aeroplane',
+    1:'automobile',
+    2:'bird',
+    3:'cat',
+    4:'deer',
+    5:'dog',
+    6:'frog',
+    7:'horse',
+    8:'ship',
+    9:'truck' 
+}
+
+#initialise GUI
+top=tk.Tk()
+top.geometry('800x600')
+top.title('State 2: Image Classification')
+top.configure(background='#CDCDCD')
+label=Label(top,background='#CDCDCD', font=('arial',15,'bold'))
+sign_image = Label(top)
+
+def classify(file_path):
+    global label_packed
+    image = Image.open(file_path)
+    image = image.resize((32,32))
+    image = numpy.expand_dims(image, axis=0)
+    image = numpy.array(image)
+    pred = model.predict_classes([image])[0]
+    sign = classes[pred]
+    print(sign)
+    label.configure(foreground='#011638', text=sign) 
+
+def show_classify_button(file_path):
+    classify_b=Button(top,text="Classify Image",
+   command=lambda: classify(file_path),padx=10,pady=5)
+    classify_b.configure(background='#364156', foreground='white',
+font=('arial',10,'bold'))
+    classify_b.place(relx=0.79,rely=0.46)
+
+def upload_image():
+    try:
+        file_path=filedialog.askopenfilename()
+        uploaded=Image.open(file_path) 
+        uploaded.thumbnail(((top.winfo_width()/2.25),
+        (top.winfo_height()/2.25)))
+        im=ImageTk.PhotoImage(uploaded)
+        sign_image.configure(image=im)
+        sign_image.image=im
+        label.configure(text='')
+        show_classify_button(file_path)
+    except:
+        pass
+
+upload=Button(top,text="Upload Image",command=upload_image,
+  padx=10,pady=5)
+
+upload.configure(background='#364156', foreground='white',
+    font=('arial',10,'bold'))
+
+upload.pack(side=BOTTOM,pady=50)
+sign_image.pack(side=BOTTOM,expand=True)
+label.pack(side=BOTTOM,expand=True)
+heading = Label(top, text="State 2: Image Classification",pady=20, font=('arial',20,'bold'))
+
+heading.configure(background='#CDCDCD',foreground='#364156')
+heading.pack()
+top.mainloop()
